93f6eadd5a0ce963469e65af7c0c0cc6797af161
KartikLakhotiya <KARTIK.LAKHOTIYA064@nmims.in>
2024-07-14 22:15:05 +0530

    solved largest element in an array

---
+++ Step 3 - Solving Problems on Array/Easy/LargestElemetInArray.py
@@ -0,0 +1,9 @@
+arr = [92, 11, 2, 34, 56, 9, 34]
+for i in range(len(arr)):
+    min_idx = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[min_idx]:
+            min_idx = j
+    arr[i], arr[min_idx] = arr[min_idx], arr[i]
+
+print(arr[-1])
